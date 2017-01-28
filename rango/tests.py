5a3277dbab1321d25949d83c06202187ed4077a8
Aidan Butler <2281611b@student.gla.ac.uk>
2017-01-28 15:09:32 +0000

    Page 95

--- rango/tests.py
+++ rango/tests.py
@@ -251,7 +251,9 @@ class Chapter7ViewTests(TestCase):
 
     # test if the add_page.html template exists.
 	
-	
+#############################################################################
+# New tests from Chapter 8
+#############################################################################	
 
 class CategoryMethodTests(TestCase):
 	def test_ensure_views_are_positive(self):
@@ -262,3 +264,31 @@ class CategoryMethodTests(TestCase):
 		cat = Category(name='test',views=-1, likes=0)
 		cat.save()
 		self.assertEqual((cat.views >= 0),True)	
+
+# Not working		
+def test_slug_line_creation(self):
+	"""
+	slug_line_creation checks to make sure that when we add
+	a category an appropriate slug line is created
+	i.e. "Random Category String" -> "random-category-string"
+	"""
+	cat = cat('Random Category String')
+	cat.save()
+	self.assertEqual(cat.slug,'random-category-string')	
+
+class IndexViewTests(TestCase):
+	def test_index_view_with_no_categories(self):
+		"""
+		If no questions exist, an appropriate message should be displayed.
+		"""
+		response = self.client.get(reverse('index'))
+		self.assertEqual(response.status_code,200)
+		self.assertContains(response, "There are no categories present.")
+		self.assertQuerysetEqual(response.context['categories'], [])	
+		
+def add_cat(name, views, likes):
+	c = Category.objects.get_or_create(name=name)[0]
+	c.views = views
+	c.likes = likes
+	c.save()
+	return	c		
